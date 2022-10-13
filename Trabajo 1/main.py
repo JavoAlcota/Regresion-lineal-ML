44e4170076a0636751e4179169c52c52dc52ba68
Francisco <panchoo99full@gmail.com>
2022-10-13 20:29:31 -0300

    Gráfico de entrenamiento listo

---
+++ Trabajo 1/main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+datos = pd.read_csv(r'data1.txt', delimiter=',', header= None)
+x = []
+y = []
+for i in datos[0]:
+    x.append(i)
+for j in datos[1]:
+    y.append(j)
+
+
+plt.scatter(x,y, color = 'red', marker='x')
+plt.title("Diagrama de dispersión de datos de entrenamiento")
+plt.xlabel("Población de la ciudad en 10.000s")
+plt.ylabel("Beneficio en $10.000s")
+plt.show()
