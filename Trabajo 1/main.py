1d2f26bd2597538f658ba90f143413776df57e39
Francisco <panchoo99full@gmail.com>
2022-10-17 20:08:11 -0300

    arreglo de los thetas. Aun asi no dan una función óptima (revisar)

--- Trabajo 1/main.py
+++ Trabajo 1/main.py
@@ -30,17 +30,18 @@ alfa = 0.01
 m = np.size(x)
 sumatoria0 = 0
 sumatoria0 = 0
-for i in range (1500):
+for i in range (1):
     for j in range (0, np.size(x)):
-        
-        sumatoria0 = sumatoria0 + (((theta0 + theta1 * X[j][1])* X[j][0] - Y[j][0])* X[j][0])
-        sumatoria1 = sumatoria0 + (((theta0 + theta1 * X[j][1])* X[j][1] - Y[j][0])* X[j][1])
+        sumatoria0 = sumatoria0 + (((theta0 + theta1 * X[j][1]) - Y[j][0])* X[j][0])
+        sumatoria1 = sumatoria0 + (((theta0 + theta1 * X[j][1]) - Y[j][0])* X[j][1])
+        print("theta0 =",theta0, theta1, X[j][1], Y[j][0], X[j][0])
+        print("theta1 =" ,theta0, theta1, X[j][1], Y[j][0], X[j][1],"\n")
     theta0 = theta0 - (alfa/m) * sumatoria0
     theta1 = theta1 - (alfa/m) * sumatoria1
 print("theta0 = ", theta0)
 print("theta1 = ", theta1)
 
-# sumatoria = 0
-# for j in range (0, np.size(x)):
-#     sumatoria = sumatoria + (((theta0 + theta1 * X[j][1]) - Y[j][0]))**2
-# print(1/(2*m)*sumatoria)
+sumatoria = 0
+for j in range (0, np.size(x)):
+    sumatoria = sumatoria + (((theta0 + theta1 * X[j][1]) - Y[j][0]))**2
+print("Función de costo =",(1/(2*m))*sumatoria)
