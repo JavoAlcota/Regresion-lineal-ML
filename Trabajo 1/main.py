610cbda2b8f26be145b1f78050d5a64e7b44dd3a
Francisco <panchoo99full@gmail.com>
2022-10-19 17:13:53 -0300

    arreglada la gradiente descendente y la funcion de coste. Falta hacer los graficos en tiempo real y los graficos en 3D

--- Trabajo 1/main.py
+++ Trabajo 1/main.py
@@ -3,7 +3,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-datos = pd.read_csv(r'C:/Users/panch/OneDrive/Escritorio/Universidad/Inteligencia Artificial/Trabajo 1/data1.txt', delimiter=',', header= None) #modificar ruta del archivo si es necesario
+# lectura y guardado de datos en los arrays "x" e "y"
+datos = pd.read_csv(r'C:/Users/panch/OneDrive/Escritorio/Trabajos/Trabajo1IA/Trabajo 1/data1.txt', delimiter=',', header= None) #modificar ruta del archivo si es necesario
 x = []
 y = []
 for i in datos[0]:
@@ -24,24 +25,22 @@ theta0 = 0
 theta1 = 0
 alfa = 0.01
 m = np.size(x)
-sumatoria0 = 0
-sumatoria0 = 0
 min = 99999999
 for i in range (1500):
+    sumatoria0 = 0
+    sumatoria1 = 0
     for j in range (0, np.size(x)):
-        sumatoria0 = sumatoria0 + (((theta0 + theta1 * X[j][1]) - Y[j][0])* X[j][0])
-        sumatoria1 = sumatoria0 + (((theta0 + theta1 * X[j][1]) - Y[j][0])* X[j][1])
-        # print("theta0 =",theta0, theta1, X[j][1], Y[j][0], X[j][0])
-        # print("theta1 =" ,theta0, theta1, X[j][1], Y[j][0], X[j][1],"\n")
-    theta0 = theta0 - (alfa/m) * sumatoria0
-    theta1 = theta1 - (alfa/m) * sumatoria1
+        hyp = theta0 + theta1 * X[j][1]
+        sumatoria0 += ((hyp - Y[j][0])* X[j][0])
+        sumatoria1 += ((hyp - Y[j][0])* X[j][1])
+    theta0 += - (alfa/m) * sumatoria0
+    theta1 += - (alfa/m) * sumatoria1
 
     #funcion de costo
     sumatoria = 0
     for j in range (0, np.size(x)):
-        sumatoria = sumatoria + (((theta0 + theta1 * X[j][1]) - Y[j][0]))**2
+        sumatoria += (((theta0 + theta1 * X[j][1]) - Y[j][0]))**2
     funcionDeCosto = (1/(2*m))*sumatoria
-    # print("Función de costo =",funcionDeCosto, "  ",theta0,theta1)
     if (funcionDeCosto < min):
         min = funcionDeCosto
         a = theta0
@@ -49,13 +48,6 @@ for i in range (1500):
 print("Funcion de costo minimo =",min)
 print("theta0 =",a)
 print("theta1 =",b)
-# print("theta0 = ", theta0)
-# print("theta1 = ", theta1)
-
-# sumatoria = 0
-# for j in range (0, np.size(x)):
-#     sumatoria = sumatoria + (((theta0 + theta1 * X[j][1]) - Y[j][0]))**2
-# print("Función de costo =",(1/(2*m))*sumatoria)
 
 yy = []
 for i in x:
